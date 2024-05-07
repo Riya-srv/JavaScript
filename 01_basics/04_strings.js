944c2db177a564800c74b5003ca7e07760c2a785
Riya-srv <67357429+Riya-srv@users.noreply.github.com>
2024-05-07 15:40:51 +0000

    string functions

---
+++ 01_basics/04_strings.js
@@ -0,0 +1,20 @@
+const name = "riya"
+const repoCount = 2
+
+//console.log (name + repoCount + "value") //Prefer not to use
+
+//String Interpolation
+console.log(`My name is ${name} and My repo count is ${repoCount}`)
+
+const gameName = new String('mario')
+
+console.log(name[1])
+console.log(name.length)
+console.log(name.toUpperCase())
+console.log(name.indexOf('a'))
+
+gameName.slice(-5,3)
+console.log(gameName.slice(-5,2))
+
+nameTrim ="  riya  "
+console.log(nameTrim.trim())
