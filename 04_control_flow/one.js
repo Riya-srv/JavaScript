4f48b1ea021a38299442b26e12eda57d516a08e8
Riya-srv <67357429+Riya-srv@users.noreply.github.com>
2024-05-15 05:32:00 +0000

    if else, switch, operators, truthy-falsy values

---
+++ 04_control_flow/one.js
@@ -0,0 +1,17 @@
+//if
+const isUserLoggedIn = true
+
+if(2 === "2"){
+ console.log("executed")
+}
+
+// < , > , <= , >= , == , != , ===
+
+const score = 200
+
+if(score > 100){
+    const power = "fly"
+    console.log(`User power ${power}`)
+}
+console.log(`User power ${power}`) //ReferenceError: power is not defined
+
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,20 @@
+//falsy values
+
+//false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy values
+
+// [],true, "0" ,"false" , " " , {} , function(){}
+
+//Nullish Coalescing Operator (??): null undefined
+
+let val1;
+//val1 = null ?? 10
+val1 = undefined ?? 15
+
+console.log(val1);
+
+
+//Ternary Operator
+
+//condition ? true : false
